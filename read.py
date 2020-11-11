f3691c22f971ceed9ad0f51600bc3fdb4f51abed
NapSpan <jorgecresposueiro@gmail.com>
2020-11-11 19:09:21 +0100

    test showing video

--- read.py
+++ read.py
@@ -1,7 +1,22 @@
-import cv2 as cv
+from cv2 import cv2 as cv
 
-img = cv.imread('imagen/gato.jpg')
+#Loads an image
+#img = cv.imread('imagen/gato.jpg')
 
-cv.imshow('Gato', img)
+#cv.imshow('Gato', img)
 
-cv.waitKey(0)
+#cv.waitKey(0)
+
+#Loads a video
+
+capture = cv.VideoCapture('video/shrek.mp4')
+
+while True:
+    isTrue, frame = capture.read()
+    cv.imshow('Video',frame)
+
+    if cv.waitKey(20) & 0xFF==ord('d'):
+        break
+
+capture.release()
+cv.destroyAllWindows()
