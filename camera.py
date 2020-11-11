631b0560be10067cbbea95cbc87b51253746f25c
NapSpan <jorgecresposueiro@gmail.com>
2020-11-11 23:33:33 +0100

    addition of custom file to work with camera

---
+++ camera.py
@@ -0,0 +1,30 @@
+from cv2 import cv2 as cv
+
+#Reescalating function works with wathever
+def rescaleFrame(frame, scale=0.75):
+    width = int(frame.shape[1] * scale)
+    height = int(frame.shape[0] * scale)
+    dimensions = (width,height)
+
+    return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
+#life video
+def changeRes(width, height):
+    capture.set(3,width)
+    capture.set(4,height)
+
+#Reading Videos
+capture = cv.VideoCapture(0)
+
+while True:
+    isTrue, frame = capture.read()
+
+    frameResized = rescaleFrame(frame, scale=0.5)
+
+    cv.imshow('Video',frame)
+    cv.imshow('VideoResized',frameResized)
+
+    if cv.waitKey(20) & 0xFF==ord('d'):
+        break
+
+capture.release()
+cv.destroyAllWindows()
