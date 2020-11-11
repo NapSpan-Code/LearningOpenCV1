d4c57a216a66c86979e22078fd6149eb94d8f6cb
NapSpan <jorgecresposueiro@gmail.com>
2020-11-11 19:27:48 +0100

    function to resize by resolution, comments

--- resizetest.py
+++ resizetest.py
@@ -5,13 +5,17 @@ from cv2 import cv2 as cv
 #cv.imshow('STALKERFAT', img)
 
 
-#Reescalating function
+#Reescalating function works with wathever
 def rescaleFrame(frame, scale=0.75):
     width = int(frame.shape[1] * scale)
     height = int(frame.shape[0] * scale)
     dimensions = (width,height)
 
     return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
+#life video
+def changeRes(width, height):
+    capture.set(3,width)
+    capture.set(4,height)
 
 #Reading Videos
 capture = cv.VideoCapture('video/shrek.mp4')
@@ -19,7 +23,7 @@ capture = cv.VideoCapture('video/shrek.mp4')
 while True:
     isTrue, frame = capture.read()
 
-    frameResized = rescaleFrame(frame)
+    frameResized = rescaleFrame(frame, scale=0.5)
 
     cv.imshow('Video',frame)
     cv.imshow('VideoResized',frameResized)
